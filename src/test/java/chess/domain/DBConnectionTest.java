1d71438e4ac8efaa60be4bb02e13de27dcf03a90
eunjungL <dldmswjd322@gmail.com>
2024-03-27 13:09:30 +0900

    feat: DB connection 구현

---
+++ src/test/java/chess/domain/DBConnectionTest.java
@@ -0,0 +1,18 @@
+package chess.domain;
+
+import chess.DBConnection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@DisplayName("데이터베이스")
+public class DBConnectionTest {
+
+    @DisplayName("데이터베이스가 제대로 연결됐는지 확인한다.")
+    @Test
+    void connection() {
+        // when & then
+        assertThatCode(DBConnection::getConnection).doesNotThrowAnyException();
+    }
+}
